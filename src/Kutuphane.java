0caa28f2fc050665fc0858ec4857931816fbc1fb
BernaBAYKAN <77546837+BernaBAYKAN@users.noreply.github.com>
2024-04-04 16:19:56 +0300

    second commit

--- src/Kutuphane.java
+++ src/Kutuphane.java
@@ -3,31 +3,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Kutuphane {
-    private List<Kitap> kitaplar = new ArrayList();
-    private List<Uye> uyeler = new ArrayList();
+    private List<Kitap> kitaplar = new ArrayList(); //Kütüphanedeki kitapların listesini tutar.
+    private List<Uye> uyeler = new ArrayList(); //Kütüphaneye üye olan kişilerin listesini tutar.
 
 
-    public void kitapEkle(Kitap kitap) {
+    public void kitapEkle(Kitap kitap) { // Kütüphaneye yeni bir kitap ekler.
 
         this.kitaplar.add(kitap);
     }
 
-    public void uyeEkle(Uye uye) {
+    public void uyeEkle(Uye uye) { //  Kütüphaneye yeni bir üye ekler.
 
         this.uyeler.add(uye);
     }
 
-    public void kitapOduncVer(String ISBN, Uye uye) {
-        Kitap kitap = (Kitap) this.kitaplar.stream().filter((k) -> {
-            return k.getISBN().equals(ISBN) && k.getDurum() == Durum.OduncAlinabilir;
-        }).findFirst().orElse(null);
+    public void kitapOduncVer(String ISBN, Uye uye) { //Bir üyeye belirtilen ISBN numaralı kitabı ödünç verir.
+        Kitap kitap = (Kitap) this.kitaplar.stream().filter((k) -> { //Kitap kitaplar listesinden ISBN numarasına göre bulunur.
+            //kitaplar listesi stream() metoduyla işlenir.her kitap için filter fonk. kullanılır. Eşleşen ilk kitap findFirst() metodu ile bulunur.
+            return k.getISBN().equals(ISBN) && k.getDurum() == Durum.OduncAlinabilir;  //Kitap ödünç alınabilir durumdaysa üyenin oduncAlinanKitaplar listesine eklenir
+        }).findFirst().orElse(null); //Eşleşen kitap bulunamadıysa null değeri döndürülür.
 
         // Hata Kontrolü ve Güvenli Dönüştürme
         if (kitap == null) {
             System.out.println("Kitap bulunamadı veya şu anda ödünç alınamaz.");
             return;
-        } else if (!(kitap instanceof Kitap)) {
-            System.out.println("Beklenmedik bir hata oluştu. Lütfen tekrar deneyin.");
+        } else if (!(kitap instanceof Kitap)) {  //findFirst() metodundan gelen değeri Kitap tipine dönüştürmeden önce instanceof operatörü kullanarak tipinin doğrulanmasını sağlar.
+            System.out.println("Beklenmedik bir hata oluştu. Lütfen tekrar deneyin."); // Kitap tipi Kitap değilse hata oluşur.
             return;
         }
 
@@ -36,8 +37,8 @@ public class Kutuphane {
 
     }
 
-    public void kitapIade(String ISBN, Uye uye) {
-        Kitap kitap = (Kitap) uye.getOduncAlinanKitaplar().stream().filter((k) -> {
+    public void kitapIade(String ISBN, Uye uye) { //kitapIade(String ISBN, Uye uye): Bir üyeden belirtilen ISBN numaralı kitabı iade alır.
+        Kitap kitap = (Kitap) uye.getOduncAlinanKitaplar().stream().filter((k) -> {  //Kitap üyenin oduncAlinanKitaplar listesinden ISBN numarasına göre bulunur.
             return k.getISBN().equals(ISBN);
         }).findFirst().orElse(null);
 
@@ -56,11 +57,12 @@ public class Kutuphane {
     }
 
     public void katalogGoster() {
-        System.out.println("Kütüphane Kataloğu:");
+        System.out.println("Kütüphane Kataloğu:"); //Kütüphanedeki tüm kitapların bilgilerini listeler.
         this.kitaplar.forEach((k) -> {
             PrintStream var10000 = System.out;
             String var10001 = k.getISBN();
             var10000.println("ISBN: " + var10001 + ", Başlık: " + k.getBaslik() + ", Yazar: " + k.getYazar() + ", Yayın Yılı: " + k.getYayinYili() + ", Durum: " + String.valueOf(k.getDurum()));
-        });
+        }); //Her kitap için, ISBN, başlık, yazar, yayın yılı ve durum bilgileri konsola yazdırılır.
+        //String.valueOf() fonksiyonu, Durum enum değerini String'e dönüştürmek için kullanılır.
     }
 }
--- src/Uye.java
+++ src/Uye.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Uye implements IUye {
-    private String isim;
-    private List<Kitap> oduncAlinanKitaplar=new ArrayList();
+    private String isim; //üyenin adı
+    private List<Kitap> oduncAlinanKitaplar=new ArrayList(); //Üyenin ödünç aldığı kitapların listesini tutar.
 
     public Uye(String isim) {
         this.isim = isim;
     }
-    public void kitapOduncAl(Kitap kitap){
-        if (kitap.getDurum() == Durum.OduncAlinabilir) {
-            kitap.setDurum(Durum.OduncVerildi);
-            this.oduncAlinanKitaplar.add(kitap);
+    public void kitapOduncAl(Kitap kitap){ //Bir kitap ödünç alır.
+        if (kitap.getDurum() == Durum.OduncAlinabilir) { //Kitabın durumu Durum.OduncAlinabilir ise:
+            kitap.setDurum(Durum.OduncVerildi); //Kitabın durumunu Durum.OduncVerildi olarak değiştirir.
+            this.oduncAlinanKitaplar.add(kitap); //Kitabı oduncAlinanKitaplar listesine ekler.
             System.out.println(kitap.getBaslik() + " kitabı ödünç alındı.");
         } else {
-            System.out.println(kitap.getBaslik() + " kitabı şu anda mevcut değil.");
+            System.out.println(kitap.getBaslik() + " kitabı şu anda mevcut değil."); //Aksi takdirde:Kitabın bu üye tarafından ödünç alınmadığını belirten mesaj gösterir.
         }
     }
     public void kitapIadeEt(Kitap kitap) {
@@ -27,12 +27,12 @@ public class Uye implements IUye {
 
     }
 
-    public List<Kitap> getOduncAlinanKitaplar() {
+    public List<Kitap> getOduncAlinanKitaplar() { //Üyenin ödünç aldığı kitapların listesini döndürür.
 
         return this.oduncAlinanKitaplar;
     }
 
-    public String getIsim() {
+    public String getIsim() { // Üyenin adını döndürür.
 
         return this.isim;
     }
